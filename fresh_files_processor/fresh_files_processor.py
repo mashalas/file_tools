6aa6953313be18b5586d5cf139bd47a106e94f12
Alexey Mashalas <noreply@example.com>
2025-06-26 19:05:40 +0800

    fresh_files_processor: конфигурационный файл читается в объект, а не в словарь

--- fresh_files_processor/fresh_files_processor.py
+++ fresh_files_processor/fresh_files_processor.py
@@ -8,10 +8,58 @@ from pprint import pprint
 import datetime
 
 
+class FFPConfig():
+    def __init__(self, filename) -> None:
+        self._filename = filename
+        self.dir = []
+        self.mask = []
+        self.processed = None
+        self.sort_by = None
+        self.sort_reverse = False
+        self.commands = []
+        with open(filename, "rt") as f:
+            reading_commands_now = False
+            for s in f:
+                s = s.strip()
+                if len(s) == 0:
+                    continue
+                if s[0] == "#":
+                    continue # комментарий
+                if s == "<commands>":
+                    # начался блок с командами операционной системы
+                    reading_commands_now = True
+                    continue
+                if s == "</commands>":
+                    # закончился блок с командами операционной системы
+                    reading_commands_now = False
+                    continue
+                if not reading_commands_now:
+                    key, value = split_key_value(s, sep="=", strip_spaces_in_key=True, strip_spaces_in_value_begin=True, strip_spaces_in_value_end=True)
+                    #print(s, key, value)
+                    if key == "dir":
+                        self.dir.append(value)
+                    elif key == "mask":
+                        self.mask.append(value)
+                    elif key == "processed":
+                        self.processed = value
+                    elif key in ["sort_by", "sort-by"]:
+                        self.sort_by = value
+                    elif key in ["sort_reverse", "sort-reverse"]:
+                        value = value.lower()
+                        if value in ["yes", "true", "1", "on"]:
+                            self.sort_reverse = True
+                        elif value in ["no", "false", "0", "off"]:
+                            self.sort_reverse = False
+                    else:
+                        print("WARNING! Unprocessable key '{}' with value '{}'" . format(key, value))
+                if reading_commands_now:
+                    self.commands.append(s)
+    # ... end of class FFPConfig ...
+
+
 def get_timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#print(get_timestamp()); exit(0)
 
 #----- Из строки вида key=value выделить ключ и значение (возможно, убрать пробелы/табуляции) -----
 def split_key_value(
@@ -43,59 +91,6 @@ def split_key_value(
 #s = "  \tparam1 =====\tvalue ";    key, value = split_key_value(s, sep="=", multiseps_as_one=True, strip_spaces_in_value_begin=True, strip_spaces_in_value_end=True)
 #print("s='{}'  key='{}'  value='{}'" . format(s, key, value))
 #exit(0)
-
-def read_config(config_path):
-    config = {
-        "dir": [],              # каталоги, из которых получить список файлов для последующей обработки
-        "mask": [],             # маски выбираемых файлов
-        "processed": None,      # файл со списком обработанных ранее файлов, чтобы к ним больше не применять обработку
-        "sort_by": None,        # критерий сортировки списка файлов
-        "sort_reverse": False,  # сортировать в обратном порядке
-
-        "commands": []          # список команд применяемых к файлам
-    }
-    
-    with open(config_path, "rt") as f:
-        reading_commands_now = False
-        for s in f:
-            s = s.strip()
-            if len(s) == 0:
-                continue
-            if s[0] == "#":
-                continue # комментарий
-            if s == "<commands>":
-                # начался блок с командами операционной системы
-                reading_commands_now = True
-                continue
-            if s == "</commands>":
-                # закончился блок с командами операционной системы
-                reading_commands_now = False
-                continue
-            if not reading_commands_now:
-                key, value = split_key_value(s, sep="=", strip_spaces_in_key=True, strip_spaces_in_value_begin=True, strip_spaces_in_value_end=True)
-                #print(s, key, value)
-                if key == "dir":
-                    config["dir"].append(value)
-                elif key == "mask":
-                    config["mask"].append(value)
-                elif key == "processed":
-                    config["processed"] = value
-                elif key in ["sort_by", "sort-by"]:
-                    config["sort_by"] = value
-                elif key in ["sort_reverse", "sort-reverse"]:
-                    value = value.lower()
-                    if value in ["yes", "true", "1", "on"]:
-                        value = True
-                    elif value in ["no", "false", "0", "off"]:
-                        value = False
-                    else:
-                        continue
-                    config["sort_reverse"] = value
-                else:
-                    print("WARNING! Unprocessable key '{}' with value '{}'" . format(key, value))
-            if reading_commands_now:
-                config["commands"].append(s)
-    return config
 
 
 def get_files_list(dirname, files_masks):
@@ -129,7 +124,6 @@ def get_files_list(dirname, files_masks):
             }
 
             found.append(elem)
-            #found.append(path)
     return found
 
 
@@ -155,57 +149,41 @@ def read_processed_files(filename):
     return result
 
 
-def file_to_array(filename):
-    result = []
-    if filename == None:
-        return result
-    if len(filename) == None:
-        return result
-    if not os.path.isfile(filename):
-        return result
-    with open(filename, "rt") as f:
-        for s in f:
-            s = s.strip()
-            if len(s) == 0:
-                continue
-            if s[0] == "#":
-                continue
-            result.append(s)
-    return result
-
-
 def process_config(config_path):
     if not os.path.isfile(config_path):
         print("File {} not found." . format(config_path))
         return False
-    config = read_config(config_path)
-    print("--- config: ---"); pprint(config); print("--- end of config ---")
+    config = FFPConfig(config_path)
+    #print("--- config: ---"); pprint(config.__dict__); print("--- end of config ---"); exit(0)
     files_list = []
-    for elem in config["dir"]:
-        files_list += get_files_list(elem, config["mask"])
-    if config["sort_by"] != None:
+    for elem in config.dir:
+        files_list += get_files_list(elem, config.mask)
+    if config.sort_by != None:
         # newlist = sorted(list_to_be_sorted, key=lambda d: d['name'])
-        print("SortBy:", config["sort_by"], "   reverse=", config["sort_reverse"], sep="")
-        files_list = sorted(files_list, key=lambda d: d[config["sort_by"]], reverse=config["sort_reverse"])
-    #print("--- files_list: ---"); pprint(files_list); print("... end of files_list ...")
-    processed_files = read_processed_files(config["processed"])
+        #print("SortBy:", config.sort_by, "   reverse=", config.sort_reverse, sep=""); exit(0)
+        files_list = sorted(files_list, key=lambda d: d[config.sort_by], reverse=config.sort_reverse)
+    #print("--- files_list: ---"); pprint(files_list); print("... end of files_list ..."); exit(0)
+    processed_files = read_processed_files(config.processed)
 
     f_processed = None
-    if config["processed"] != None and len(config["processed"]) > 0:
-        f_processed = open(config["processed"], "at")
+    if config.processed != None and len(config.processed) > 0:
+        f_processed = open(config.processed, "at")
     for one_file_dict in files_list:
         if one_file_dict["longname"] in processed_files:
             # файл уже обрабатывался ранее
             print("skip file '{}' because it was processed earlier" . format(one_file_dict["longname"]))
             continue
-        print(one_file_dict)
-        for one_cmd in config["commands"]:
+        #print(one_file_dict)
+        print("process '{}'" . format(one_file_dict["longname"]))
+        for one_cmd in config.commands:
             for key in one_file_dict.keys():
                 if type(one_file_dict[key]) != type("abc"):
+                    # если атрибут файла не является строкой - сделать его строкой, чтобы можно было использовать в качестве значения переменной
                     one_file_dict[key] = str(one_file_dict[key])
                 one_cmd = one_cmd.replace("${" + key + "}", one_file_dict[key])
-            print("CMD:", one_cmd)
+            print("  ", one_cmd)
             os.system(one_cmd)
+        print("")
         
         if f_processed != None:
             f_processed.write(
@@ -220,7 +198,7 @@ def process_config(config_path):
 
 
 if __name__ == "__main__":
-    process_config("config1.cfg"); exit(0)
+    #process_config("config1.cfg"); exit(0)
     for i in range(1, len(sys.argv)):
         process_config(sys.argv[i])
     
